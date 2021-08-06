2ce8368542b5f35d7ac8091bc9f7c44380e318f4
deepaksharma01 <60283312+deepaksharma01@users.noreply.github.com>
2021-08-06 20:19:18 +0530

    upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calculator()
+{
+    let a = Number(document.querySelector("#val1").value)
+    let b = Number(document.querySelector("#val2").value)
+
+    document.querySelector("#result").innerHTML = "<h1>Output:<h1>"
+
+    console.log(document.querySelector("#sum").innerText = `${a} + ${b} = ${a+b}`)
+    console.log(document.querySelector("#sub").innerText = `${a} - ${b} = ${a-b}`)
+    console.log(document.querySelector("#mul").innerText = `${a} * ${b} = ${a*b}`)
+    console.log(document.querySelector("#div").innerText = `${a} / ${b} = ${a/b}`)
+    
+}
