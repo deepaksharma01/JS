3374dc13ae53dd7747f30c73430bfa0b111ac7df
deepaksharma01 <60283312+deepaksharma01@users.noreply.github.com>
2021-08-07 16:39:18 +0530

    update

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,19 +1,24 @@
-let inputs = document.querySelectorAll('input')
-let btnclear = document.querySelector('#btn2')
-btnclear.addEventListener('click', () =>{
-    inputs.forEach(input => input.value = '')
-})
-
-function calculator()
+function calculate()
 {
-    let a = Number(document.querySelector("#val1").value)
-    let b = Number(document.querySelector("#val2").value)
+     let a = Number(document.querySelector("#val1").value)
+     let b = Number(document.querySelector("#val2").value)
 
-    document.querySelector("#result").innerHTML = "<h1>Output:<h1>"
+     document.querySelector("#result").innerHTML = "<h1>Output:<h1>"
 
-    console.log(document.querySelector("#sum").innerText = `${a} + ${b} = ${a+b}`)
-    console.log(document.querySelector("#sub").innerText = `${a} - ${b} = ${a-b}`)
-    console.log(document.querySelector("#mul").innerText = `${a} * ${b} = ${a*b}`)
-    console.log(document.querySelector("#div").innerText = `${a} / ${b} = ${a/b}`)
-    
+     console.log(document.querySelector("#sum").innerText = `${a} + ${b} = ${a+b}`)
+     console.log(document.querySelector("#sub").innerText = `${a} - ${b} = ${a-b}`)
+     console.log(document.querySelector("#mul").innerText = `${a} * ${b} = ${a*b}`)
+     console.log(document.querySelector("#div").innerText = `${a} / ${b} = ${a/b}`)
+
+}
+
+function clean()
+{
+     document.querySelector("#val1").value = ""
+     document.querySelector("#val2").value = ""
+     document.querySelector("#result").innerHTML = " "
+     document.querySelector("#sum").innerText = ` `
+     document.querySelector("#sub").innerText = ` `
+     document.querySelector("#mul").innerText = ` `
+     document.querySelector("#div").innerText = ` `
 }
