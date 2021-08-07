5768d77928c7535e9e3ebd2988dfb19ab05c46ac
deepaksharma01 <60283312+deepaksharma01@users.noreply.github.com>
2021-08-07 19:02:05 +0530

    update

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -13,18 +13,34 @@ function reverse()
           rev.push(arr[i])
      }
 
-     document.querySelector("#result").innerHTML = `<h1>Output:<h1>`
+     document.querySelector("#result1").innerHTML = `<h1>Output:<h1>`
      document.querySelector("#arr").innerHTML = `Array: <br> [${arr}]`
      document.querySelector("#rev").innerHTML = `Reversed Array: <br> [${rev}]`
 
 
 }
 
+function divisibility()
+{
+     let n = (document.querySelector("#val2").value)
+     let arr = []
+     for(i=0;i<=n;i++){
+          if (i%5==0)
+          {
+          arr.push(i)
+          }
+     }
+     document.querySelector("#result2").innerHTML = `<h1>Output:<h1>`
+     document.querySelector("#numb").innerHTML = `Numbers divisible by 5 from 1 to ${n} are: <br> [${arr}]`
+
+}
 function clean()
 {
      document.querySelector("#val1").value = ""
-     document.querySelector("#result").innerHTML = " "
+     document.querySelector("#result1").innerHTML = " "
      document.querySelector("#arr").innerHTML = ` `
      document.querySelector("#rev").innerHTML = ` `
 
+     document.querySelector("#result2").innerHTML = " "
+     document.querySelector("#numb").innerHTML = ` `
 }
